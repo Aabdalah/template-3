4c231339fe6a90902f2c0ff7920d47115364930b
Aabdalah <115105760+Aabdalah@users.noreply.github.com>
2024-05-25 10:33:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ let firstEl = document.querySelector(".meter-container:nth-child(1) .front")
 let second = document.querySelector(".meter-container:nth-child(2) .front")
 let third = document.querySelector(".meter-container:nth-child(3) .front")
 let fourth = document.querySelector(".meter-container:nth-child(4) .front")
-console.log(firstEl)
+
 
 window.addEventListener("scroll",function(){
-    console.log(ourSkills.getBoundingClientRect().top)
     if(ourSkills.getBoundingClientRect().top < 300){
         firstEl.style.width = "80%"
         second.style.width = "85%"
@@ -14,3 +13,84 @@ window.addEventListener("scroll",function(){
         fourth.style.width = "80%"
     }
 })
+
+let containers = document.querySelectorAll(".square");
+
+containers.forEach((el)=>{
+    for(let i = 0 ; i < 132 ; i++){
+        let div = document.createElement("div");
+        el.append(div);
+    }
+})
+
+let date = new Date();
+date.setFullYear(2025);
+date.setMonth(0);
+date.setDate(1);
+date.setHours(0)
+date.setMinutes(0)
+date.setSeconds(0)
+date.setMilliseconds(0)
+
+function getdays(){
+    return (Date.parse(date)-Date.now())/1000/60/60/24;
+}
+function gethours(){
+    return Number("0."+String(getdays()).split(".")[1])*24;
+}
+function getminutes(){
+    return Number("0."+String(gethours()).split(".")[1])*60;
+}
+function getseconds(){
+    return Number("0."+String(getminutes()).split(".")[1])*60;
+}
+
+let day = document.querySelector(".day");
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let sec = document.querySelector(".second");
+
+if(getdays() >= 10){
+    day.textContent = parseInt(getdays())
+}else{
+    day.textContent = "0" + parseInt(getdays())
+}
+if(getseconds() >= 10){
+    sec.textContent = parseInt(getseconds())
+}else{
+    sec.textContent = "0" + parseInt(getseconds())
+}
+if(getminutes() >= 10){
+    minute.textContent = parseInt(getminutes())
+}else{
+    minute.textContent = "0" + parseInt(getminutes())
+}
+if(gethours() >= 10){
+    hour.textContent = parseInt(gethours())
+}else{
+    hour.textContent = "0" + parseInt(gethours())
+}
+
+setInterval(() => {
+    if(getseconds() >= 10){
+        sec.textContent = parseInt(getseconds())
+    }else{
+        sec.textContent = "0" + parseInt(getseconds())
+    }
+    if(getminutes() >= 10){
+        minute.textContent = parseInt(getminutes())
+    }else{
+        minute.textContent = "0" + parseInt(getminutes())
+    }
+    if(gethours() >= 10){
+        hour.textContent = parseInt(gethours())
+    }else{
+        hour.textContent = "0" + parseInt(gethours())
+    }
+    if(getdays() >= 10){
+        day.textContent = parseInt(getdays())
+    }else{
+        day.textContent = "0" + parseInt(getdays())
+    }
+    
+}, 1000);
